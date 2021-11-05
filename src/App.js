3f4015c2580998882f64102a8f98231614a2c4e3
Sara Pulido <sara01rizo@gmail.com>
2021-11-05 21:50:05 +0100

    Init react weather app project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>React weather app </h1>
+        <h2>Hello World!!!</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
