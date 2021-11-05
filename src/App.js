f49cd4cdf5661b972f084de467d4e350c290a6c6
Sara Pulido <sara01rizo@gmail.com>
2021-11-05 22:17:54 +0100

    Added styles CSS file -

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import "./App.css";
+import React from "react";
+import WeatherApp from "./WeatherApp";
+import "./styles.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>React weather app </h1>
-        <h2>Hello World!!!</h2>
+        <h2>The weather always change!!!</h2>
       </header>
+      <body>
+        <WeatherApp />
+      </body>
     </div>
   );
 }
